703b7dfc684ca19c7394f511a9bc79c0274663d3
Vadim <44135514+vodemn@users.noreply.github.com>
2023-03-03 19:10:41 +0300

    more exports

--- lib/m3_lightmeter_resources.dart
+++ lib/m3_lightmeter_resources.dart
@@ -6,3 +6,7 @@ export 'src/data/models/photography_values/iso_value.dart';
 export 'src/data/models/photography_values/nd_value.dart';
 export 'src/data/models/photography_values/photography_value.dart';
 export 'src/data/models/photography_values/shutter_speed_value.dart';
+
+export 'src/res/dimens.dart';
+
+export 'src/utils/log_2.dart';
