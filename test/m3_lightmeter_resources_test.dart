c21f80177e6d11b8a7909b0f05e82c0058b0cfc0
Vadim <vadim.turko@gmail.com>
2023-04-01 22:11:56 +0300

    Moved value-lists to the corresponding classes

--- test/m3_lightmeter_resources_test.dart
+++ test/m3_lightmeter_resources_test.dart
@@ -4,35 +4,35 @@ import 'package:m3_lightmeter_resources/m3_lightmeter_resources.dart';
 void main() {
   // Stringify
   test('Stringify aperture values', () {
-    expect(apertureValues.first.toString(), "f/1.0");
-    expect(apertureValues.last.toString(), "f/45");
+    expect(ApertureValue.values.first.toString(), "f/1.0");
+    expect(ApertureValue.values.last.toString(), "f/45");
   });
 
   test('Stringify iso values', () {
-    expect(isoValues.first.toString(), "3");
-    expect(isoValues.last.toString(), "6400");
+    expect(IsoValue.values.first.toString(), "3");
+    expect(IsoValue.values.last.toString(), "6400");
   });
 
   test('Stringify shutter speed values', () {
-    expect(shutterSpeedValues.first.toString(), "1/2000");
-    expect(shutterSpeedValues.last.toString(), "16\"");
+    expect(ShutterSpeedValue.values.first.toString(), "1/2000");
+    expect(ShutterSpeedValue.values.last.toString(), "16\"");
   });
 
   // Stops
   test('Aperture values stops lists', () {
-    expect(apertureValues.fullStops().length, 12);
-    expect(apertureValues.halfStops().length, 12 + 11);
-    expect(apertureValues.thirdStops().length, 12 + 22);
+    expect(ApertureValue.values.fullStops().length, 12);
+    expect(ApertureValue.values.halfStops().length, 12 + 11);
+    expect(ApertureValue.values.thirdStops().length, 12 + 22);
   });
 
   test('Iso values stops lists', () {
-    expect(isoValues.fullStops().length, 12);
-    expect(isoValues.thirdStops().length, 12 + 22);
+    expect(IsoValue.values.fullStops().length, 12);
+    expect(IsoValue.values.thirdStops().length, 12 + 22);
   });
 
   test('Shutter speed values stops lists', () {
-    expect(shutterSpeedValues.fullStops().length, 16);
-    expect(shutterSpeedValues.halfStops().length, 16 + 15);
-    expect(shutterSpeedValues.thirdStops().length, 16 + 30);
+    expect(ShutterSpeedValue.values.fullStops().length, 16);
+    expect(ShutterSpeedValue.values.halfStops().length, 16 + 15);
+    expect(ShutterSpeedValue.values.thirdStops().length, 16 + 30);
   });
 }
