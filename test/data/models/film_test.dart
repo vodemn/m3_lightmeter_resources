79c6b4f0d9f3d6e03b53dec8620566002bb4a958
Vadim <vadim.turko@gmail.com>
2024-10-01 13:39:46 +0200

    fixed `Film` test coverage

--- test/data/models/film_test.dart
+++ test/data/models/film_test.dart
@@ -4,6 +4,10 @@ import 'package:m3_lightmeter_resources/src/data/models/photography_values/photo
 import 'package:m3_lightmeter_resources/src/data/models/photography_values/shutter_speed_value.dart';
 
 void main() {
+  const a = Film(name: 'Film 1', iso: 100);
+  const b = Film(name: 'Film 1', iso: 100);
+  const c = Film(name: 'Film 2', iso: 100);
+
   test('iso', () {
     expect(const Film.other().iso, 0);
   });
@@ -14,32 +18,56 @@ void main() {
 
   test('==', () {
     expect(const Film.other() == Film.values[0], true);
-    expect(const Film(name: 'Film 1', iso: 100) == const Film(name: 'Film 1', iso: 100), true);
-    expect(const Film(name: 'Film 1', iso: 100) == const Film.other(), false);
-    expect(const Film(name: 'Film 1', iso: 100) == const Film(name: 'Film 2', iso: 100), false);
+    expect(a == b, true);
+    expect(a == const Film.other(), false);
+    expect(a == c, false);
   });
 
   test('hashCode', () {
     expect(const Film.other().hashCode == Film.values[0].hashCode, true);
-    expect(const Film(name: 'Film 1', iso: 100).hashCode == const Film(name: 'Film 1', iso: 100).hashCode, true);
-    expect(const Film(name: 'Film 1', iso: 100).hashCode == const Film.other().hashCode, false);
-    expect(const Film(name: 'Film 1', iso: 100).hashCode == const Film(name: 'Film 2', iso: 100).hashCode, false);
+    expect(a.hashCode == b.hashCode, true);
+    expect(a.hashCode == const Film.other().hashCode, false);
+    expect(a.hashCode == c.hashCode, false);
   });
 
-  test(
+  group(
     'reciprocityFailure',
     () {
-      expect(
-        const Film.other().reciprocityFailure(const ShutterSpeedValue(1000, true, StopType.full)),
-        const ShutterSpeedValue(1000, true, StopType.full),
+      test(
+        'no formula',
+        () {
+          expect(
+            const Film.other().reciprocityFailure(const ShutterSpeedValue(1000, true, StopType.full)),
+            const ShutterSpeedValue(1000, true, StopType.full),
+          );
+          expect(
+            const Film.other().reciprocityFailure(const ShutterSpeedValue(1, false, StopType.full)),
+            const ShutterSpeedValue(1, false, StopType.full),
+          );
+          expect(
+            const Film.other().reciprocityFailure(const ShutterSpeedValue(16, false, StopType.full)),
+            const ShutterSpeedValue(16, false, StopType.full),
+          );
+        },
       );
-      expect(
-        const Film.other().reciprocityFailure(const ShutterSpeedValue(1, false, StopType.full)),
-        const ShutterSpeedValue(1, false, StopType.full),
-      );
-      expect(
-        const Film.other().reciprocityFailure(const ShutterSpeedValue(16, false, StopType.full)),
-        const ShutterSpeedValue(16, false, StopType.full),
+
+      test(
+        'has formula',
+        () {
+          final film = Film(name: 'Film 2', iso: 100, reciprocityFormula: (t) => t * 2);
+          expect(
+            film.reciprocityFailure(const ShutterSpeedValue(1000, true, StopType.full)),
+            const ShutterSpeedValue(1000, true, StopType.full),
+          );
+          expect(
+            film.reciprocityFailure(const ShutterSpeedValue(1, false, StopType.full)),
+            const ShutterSpeedValue(2, false, StopType.full),
+          );
+          expect(
+            film.reciprocityFailure(const ShutterSpeedValue(16, false, StopType.full)),
+            const ShutterSpeedValue(32, false, StopType.full),
+          );
+        },
       );
     },
   );
