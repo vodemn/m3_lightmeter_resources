c9ca95ad24e4c9246e0c19eba40ef16851796752
Vadim <vadim.turko@gmail.com>
2024-10-01 13:22:52 +0200

    implemented `CustomFilm`

--- lib/src/data/models/film.dart
+++ lib/src/data/models/film.dart
@@ -1,9 +1,11 @@
 import 'package:m3_lightmeter_resources/m3_lightmeter_resources.dart';
 
+typedef ReciprocityFormula = double Function(double value);
+
 class Film {
   final String name;
   final int iso;
-  final double Function(double value)? reciprocityFormula;
+  final ReciprocityFormula? reciprocityFormula;
 
   const Film({
     required this.name,
@@ -32,4 +34,41 @@ class Film {
   }
 
   static const List<Film> values = [Film.other()];
+}
+
+class CustomFilm extends Film {
+  final String id;
+
+  const CustomFilm({
+    required this.id,
+    required super.name,
+    required super.iso,
+    super.reciprocityFormula,
+  });
+
+  CustomFilm copyWith({
+    String? name,
+    int? iso,
+    ReciprocityFormula? reciprocityFormula,
+  }) =>
+      CustomFilm(
+        id: id,
+        name: name ?? this.name,
+        iso: iso ?? this.iso,
+        reciprocityFormula: reciprocityFormula ?? this.reciprocityFormula,
+      );
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other.runtimeType != runtimeType) return false;
+    return other is CustomFilm &&
+        other.id == id &&
+        other.name == name &&
+        other.iso == iso &&
+        other.reciprocityFormula == reciprocityFormula;
+  }
+
+  @override
+  int get hashCode => Object.hash(id, name, iso, runtimeType);
 }
